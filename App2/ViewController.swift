01ba03dac9493eb0912688b765d3100fc4843231
Makeda Gebremdhin <makeda@lbl.gov>
2016-09-29 12:22:46 -0700

    Initial Commit

---
+++ App2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App2
+//
+//  Created by Makeda Gebremedhin on 9/29/16.
+//  Copyright © 2016 Makeda Gebremedhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
